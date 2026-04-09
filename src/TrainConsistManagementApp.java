84fdc6e05c047f5584b5ca9d3ec01dc34f5e0a0e
jayagrawal-petabyte <jayagrawal72511@gmail.com>
2026-04-09 16:29:22 +0530

    resolve

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,39 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
-// Bogie class
-class Bogie {
-    String name;
-    int capacity;
-
-    Bogie(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
-
-    @Override
-    public String toString() {
-        return name + " -> " + capacity;
-    }
-}
 
 public class TrainConsistManagementApp {
 
-    public static void main(String[] args) {
-
-        System.out.println("=== Train Consist Management App ===");
-
-        List<Bogie> bogies = new ArrayList<>();
-        bogies.add(new Bogie("Sleeper", 72));
-        bogies.add(new Bogie("AC Chair", 60));
-        bogies.add(new Bogie("First Class", 24));
-
-        List<Bogie> filteredBogies = bogies.stream()
-                .filter(b -> b.capacity > 60)
-                .collect(Collectors.toList());
-
-        System.out.println("\nFiltered Bogies (capacity > 60):");
-        filteredBogies.forEach(System.out::println);
-    }
 }
